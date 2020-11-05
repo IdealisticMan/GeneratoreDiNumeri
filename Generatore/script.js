e60647e4e7510a87d4f329e4fd07070f6f74ddd1
IdealisticMan <73000472+IdealisticMan@users.noreply.github.com>
2020-11-05 19:52:41 +0100

    Delete script.js

--- Generatore/script.js
+++
@@ -1,6 +0,0 @@
-// JavaScript Document
-function Random() { 
-    var notRandomNumbers = [10,11,6,7]; 
-    var rnd = Math.floor(Math.random() * notRandomNumbers.length); 
-    document.getElementById('tb').value = notRandomNumbers[rnd]; 
-}
